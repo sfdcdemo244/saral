f4a3ebf0b95f280d7e59fa9a1d81be9bf5e2e0d4
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-16 12:35:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en" class="pink-theme">
+
+
+<!-- Mirrored from maxartkiller.com/website/GoMobileUX/gofruit/splash.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 07 Dec 2019 03:00:21 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="Maxartkiller">
+
+    <title></title>
+    
+    <!-- Material design icons CSS -->
+    <link rel="stylesheet" href="vendor/materializeicon/material-icons.css">
+
+    <!-- Roboto fonts CSS -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap-4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Swiper CSS -->
+    <link href="vendor/swiper/css/swiper.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="row no-gutters vh-100">
+        <div class="col align-self-center text-white text-center">
+            <img src="img/logo1.png" alt="logo">
+            <h1><span class="font-weight-light"></span>SARAL</h1>
+            <div class="laoderhorizontal"><div></div><div></div><div></div><div></div></div>
+        </div>
+    </div>
+
+    <!-- jquery, popper and bootstrap js -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="vendor/bootstrap-4.3.1/js/bootstrap.min.js"></script>
+
+    <!-- swiper js -->
+    <script src="vendor/swiper/js/swiper.min.js"></script>
+    
+    <!-- template custom js -->
+    <script src="js/main.js"></script>
+    
+    <script>
+        $(window).on('load', function(){
+            setTimeout(function(){
+                window.open("introduction.html", "_self");    
+            }, 2000)
+            
+        })
+    
+    </script>
+</body>
+
+
+<!-- Mirrored from maxartkiller.com/website/GoMobileUX/gofruit/splash.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 07 Dec 2019 03:00:39 GMT -->
+</html>
