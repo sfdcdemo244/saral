9246cece437aae72e851a29556318cb84c71c828
sfdcdemo244 <80699562+sfdcdemo244@users.noreply.github.com>
2021-03-16 12:26:24 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,35 @@
+    
+        <div class="footer">
+            <div class="no-gutters">
+                <div class="col-auto mx-auto">
+                    <div class="row no-gutters justify-content-center">
+                        <div class="col-auto">
+                            <a href="home.php" class="btn btn-link-default active">
+                                <i class="material-icons">home</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="notification.php" class="btn btn-link-default">
+                                <i class="material-icons">info</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="total-book.php" class="btn btn-default shadow centerbutton">
+                                <i class=" fa fa-stethoscope" style="font-size: 18px; font-weight: bold;"></i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="history.php" class="btn btn-link-default">
+                                <i class="material-icons">history</i>
+                            </a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="profile.php" class="btn btn-link-default">
+                                <i class="material-icons">account_circle</i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ inc/header.php
@@ -0,0 +1,42 @@
+ <div class="row no-gutters vh-100 loader-screen">
+        <div class="col align-self-center text-white text-center">
+            <img src="img/logo1.png" alt="logo">
+            <h1><span class="font-weight-light"></span>Saral</h1>
+            <div class="laoderhorizontal"><div></div><div></div><div></div><div></div></div>
+        </div>
+    </div>
+<div class="sidebar">
+        <div class="text-center">
+            <div class="figure-menu shadow">
+                <figure><img src="img/user1.png" alt=""></figure>
+            </div>
+            <h5 class="mb-1 ">Shruti</h5>
+            <p class="text-mute small"></p>
+        </div>
+        <br>
+        
+                <h5 class="subtitle text-uppercase"><span>Menu</span></h5>
+                <div class="list-group main-menu">
+                    <a href="profile.php" class="list-group-item list-group-item-action active">My Profile</a>
+                    <a href="total-book.php" class="list-group-item list-group-item-action">Live Queue <span class="badge badge-dark text-white">2</span></a>
+                    <a href="history.php" class="list-group-item list-group-item-action">History</a>
+                    <a href="notification.php" class="list-group-item list-group-item-action">Info</a>
+                     <a href="#" class="list-group-item list-group-item-action">Notification</a>
+                    <a href="setting.php" class="list-group-item list-group-item-action">Settings</a>
+                    <a href="login.html" class="list-group-item list-group-item-action mt-4">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper">
+        <div class="header">
+            <div class="row no-gutters">
+                <div class="col-auto">
+                    <button class="btn  btn-link text-dark menu-btn"><img src="img/menu.png" alt=""><span class="new-notification"></span></button>
+                </div>
+                <div class="col text-center"><img src="img/logo1.png" alt="" class="header-logo"> </div>
+                <div class="col-auto">
+                    <a href="#" class="btn  btn-link text-dark"><i class="fa fa-bell"></i></a>
+                </div>
+            </div>
+        </div>
